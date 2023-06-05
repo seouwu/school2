2e679a18e657b3943eeae891744b19dbcd9eb4e4
seouwu <127573597+seouwu@users.noreply.github.com>
2023-06-05 12:23:00 +0900

    Add files via upload

--- seouwuscript.js
+++ seouwuscript.js
@@ -3,11 +3,11 @@ function myname() {
     let userAnswer = document.getElementById("mynameinput").value; // 사용자가 입력한 답
   
     // 정답과 비교
-    if (userAnswer === "SEOWOO") {
+    if (userAnswer == "SEOWOO") {
         alert("맞았습니다!");}
-    if (userAnswer === "서우") {
+    if (userAnswer == "서우") {
         alert("맞았습니다!");}
-    if (userAnswer === "seowoo") {
+    if (userAnswer == "seowoo") {
         alert("맞았습니다!");
     } else {
         alert("틀렸습니다!");
@@ -17,9 +17,9 @@ function myname() {
     let userAnswer = document.getElementById("catnameinput").value; // 사용자가 입력한 답
   
     // 정답과 비교
-    if (userAnswer === "설") {
+    if (userAnswer == "설") {
         alert("맞았습니다!");}
-    if (userAnswer === "설이") {
+    if (userAnswer == "설이") {
         alert("맞았습니다!");
     } else {
         alert("틀렸습니다!");
