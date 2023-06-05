1298dc8163a12cd7bfdaca81ed78fcdc6dde534b
seouwu <127573597+seouwu@users.noreply.github.com>
2023-06-05 12:39:33 +0900

    Add files via upload

--- seouwuscript.js
+++ seouwuscript.js
@@ -3,12 +3,12 @@ function myname() {
     let userAnswer = document.getElementById("mynameinput").value; // 사용자가 입력한 답
   
     // 정답과 비교
-    if (userAnswer == "SEOWOO"||"서우"||"seowoo") {
+    if (userAnswer === "SEOWOO") {
         alert("맞았습니다!");}
-    // if (userAnswer == "서우") {
-    //     alert("맞았습니다!");}
-    // if (userAnswer == "seowoo") {
-    //     alert("맞았습니다!");} 
+    else if (userAnswer === "서우") {
+        alert("맞았습니다!");}
+    else if (userAnswer === "seowoo") {
+        alert("맞았습니다!");} 
     else {
         alert("틀렸습니다!");
     }
@@ -17,10 +17,10 @@ function myname() {
     let userAnswer = document.getElementById("catnameinput").value; // 사용자가 입력한 답
   
     // 정답과 비교
-    if (userAnswer == "설"|| "설이") {
+    if (userAnswer === "설") {
         alert("맞았습니다!");}
-    // if (userAnswer == "설이") {
-    //     alert("맞았습니다!");} 
+    else if (userAnswer == "설이") {
+        alert("맞았습니다!");} 
     else {
         alert("틀렸습니다!");
     }
